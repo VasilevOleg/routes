0f66b4e19827ab74f10256860ddabd4179e121d0
Oleg <100127120+VasilevOleg@users.noreply.github.com>
2025-10-05 11:09:12 +0400

    Create my_routes.bat

---
+++ my_routes.bat
@@ -0,0 +1,11 @@
+-- satellitetoday.com
+route add 23.185.0.2 mask 255.255.255.255 0.0.0.0
+-- imagetmdb.cub.red
+route add 117.55.203.183 mask 255.255.255.255 0.0.0.0
+-- cub.red
+route add 5.61.53.100 mask 255.255.255.255 0.0.0.0
+-- www.esa.int
+route add 151.101.2.132 mask 255.255.255.255 0.0.0.0
+route add 151.101.130.132 mask 255.255.255.255 0.0.0.0
+route add 151.101.194.132 mask 255.255.255.255 0.0.0.0
+route add 151.101.66.132 mask 255.255.255.255 0.0.0.0
