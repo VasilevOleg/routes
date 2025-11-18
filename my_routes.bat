659b1821a812581ce08ad004f6906f600dae89f6
Oleg <100127120+VasilevOleg@users.noreply.github.com>
2025-11-18 11:23:13 +0400

    upd visual

--- my_routes.bat
+++ my_routes.bat
@@ -1,41 +1,32 @@
--- satellitetoday.com
-route add 23.185.0.2 mask 255.255.255.255 0.0.0.0
--- imagetmdb.cub.red
-route add 117.55.203.183 mask 255.255.255.255 0.0.0.0
--- cub.red
-route add 5.61.53.100 mask 255.255.255.255 0.0.0.0
--- www.esa.int
-route add 151.101.2.132 mask 255.255.255.255 0.0.0.0
-route add 151.101.130.132 mask 255.255.255.255 0.0.0.0
-route add 151.101.194.132 mask 255.255.255.255 0.0.0.0
-route add 151.101.66.132 mask 255.255.255.255 0.0.0.0
-route add 178.32.232.86 mask 255.255.255.255 0.0.0.0
--- vdsina.com
-route add 5.252.32.139 mask 255.255.255.255 0.0.0.0
--- iridium.com
-route add 13.33.235.34 mask 255.255.255.255 0.0.0.0
-route add 13.33.235.47 mask 255.255.255.255 0.0.0.0
-route add 13.33.235.104 mask 255.255.255.255 0.0.0.0
-route add 13.33.235.54 mask 255.255.255.255 0.0.0.0
-route add 18.239.83.13 mask 255.255.255.255 0.0.0.0
-route add 18.239.83.50 mask 255.255.255.255 0.0.0.0
-route add 18.239.83.41 mask 255.255.255.255 0.0.0.0
-route add 18.239.83.92 mask 255.255.255.255 0.0.0.0
-route add 76.223.57.34 mask 255.255.255.255 0.0.0.0
-route add 15.197.239.18 mask 255.255.255.255 0.0.0.0
--- spacenews.com
-route add 192.0.78.24 mask 255.255.255.255 0.0.0.0
-route add 192.0.78.25 mask 255.255.255.255 0.0.0.0
-route add 185.131.64.122 mask 255.255.255.255 0.0.0.0
-route add 84.17.46.53 mask 255.255.255.255 0.0.0.0
-route add 199.232.192.134 mask 255.255.255.255 0.0.0.0
-route add 199.232.196.134 mask 255.255.255.255 0.0.0.0
-route add 151.101.128.134 mask 255.255.255.255 0.0.0.0
-route add 151.101.64.134 mask 255.255.255.255 0.0.0.0
-route add 151.101.0.134 mask 255.255.255.255 0.0.0.0
-route add 151.101.192.134 mask 255.255.255.255 0.0.0.0
--- make.com
-route add 104.18.37.111 mask 255.255.255.255 0.0.0.0
-route add 172.64.150.145 mask 255.255.255.255 0.0.0.0
--- rockblack.pro
-route add 89.110.102.246 mask 255.255.255.255 0.0.0.0
+route add 23.185.0.2 mask 255.255.255.255 0.0.0.0 #satellitetoday.com
+route add 117.55.203.183 mask 255.255.255.255 0.0.0.0 #imagetmdb.cub.red
+route add 5.61.53.100 mask 255.255.255.255 0.0.0.0 #cub.red
+route add 151.101.2.132 mask 255.255.255.255 0.0.0.0 #esa.int
+route add 151.101.130.132 mask 255.255.255.255 0.0.0.0 #esa.int
+route add 151.101.194.132 mask 255.255.255.255 0.0.0.0 #esa.int
+route add 151.101.66.132 mask 255.255.255.255 0.0.0.0 #esa.int
+route add 178.32.232.86 mask 255.255.255.255 0.0.0.0 #esa.int
+route add 5.252.32.139 mask 255.255.255.255 0.0.0.0 #vdsina.com
+route add 13.33.235.34 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 13.33.235.47 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 13.33.235.104 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 13.33.235.54 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 18.239.83.13 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 18.239.83.50 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 18.239.83.41 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 18.239.83.92 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 76.223.57.34 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 15.197.239.18 mask 255.255.255.255 0.0.0.0 #iridium.com
+route add 192.0.78.24 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 192.0.78.25 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 185.131.64.122 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 84.17.46.53 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 199.232.192.134 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 199.232.196.134 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 151.101.128.134 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 151.101.64.134 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 151.101.0.134 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 151.101.192.134 mask 255.255.255.255 0.0.0.0 #spacenews.com
+route add 104.18.37.111 mask 255.255.255.255 0.0.0.0 #make.com
+route add 172.64.150.145 mask 255.255.255.255 0.0.0.0 #make.com
+route add 89.110.102.246 mask 255.255.255.255 0.0.0.0 #rockblack.pro
